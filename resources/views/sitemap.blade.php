9a97d59873de8454bf8cafec0b52b3ad7d90773d
l0c4lxid <syaidxandhika@gmail.com>
2025-12-02 09:54:52 +0700

    fix seov2

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,18 +1,19 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+{!! '<' . '?xml version="1.0" encoding="UTF-8"?>' !!}
+@php $home = rtrim($base ?? config('app.url'), '/'); @endphp
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-        <loc>{{ $base }}/</loc>
+        <loc>{{ $home }}/</loc>
         <changefreq>weekly</changefreq>
         <priority>1.0</priority>
     </url>
     <url>
-        <loc>{{ $base }}{{ route('blog.index', [], false) }}</loc>
+        <loc>{{ $home }}{{ route('blog.index', [], false) }}</loc>
         <changefreq>weekly</changefreq>
         <priority>0.9</priority>
     </url>
     @foreach ($posts as $post)
         <url>
-            <loc>{{ $base }}{{ route('blog.show', $post, false) }}</loc>
+            <loc>{{ $home }}{{ route('blog.show', $post, false) }}</loc>
             @if ($post->published_at)
                 <lastmod>{{ optional($post->published_at)->format('Y-m-d') }}</lastmod>
             @endif
